6291c19339239965d24405a427a21839a878f519
Jonathan <76789404+JOPlatt@users.noreply.github.com>
2023-05-11 15:58:06 -0600

    Delete drtaV3 directory

--- drtaV3/Sandbox.m
+++
@@ -1,4 +0,0 @@
-
-Dlist = "Hello"; % [{"this is one"},{"this is two"},{"this is three"}];
-Dlist = py.numpy.int64(2);
-pyrunfile('NWBConvert.py',A=Dlist)
--- drtaV3/drtaPullThreshold.m
+++
@@ -1,6 +0,0 @@
-function [drta_p]= drtaPullThreshold
-
-
-[FileName,PathName] = uigetfile('jt_times*.mat','Select jt_times file to pull threshold');
-load([PathName,FileName])
-
