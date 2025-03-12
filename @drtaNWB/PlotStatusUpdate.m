8f711fe89e0b7e29e0cd599273ec68fea47ff553
PlattJo <jonathan.platt@cuanschutz.edu>
2025-03-12 10:13:15 -0600

    adding diff plot status update text block

---
+++ @drtaNWB/PlotStatusUpdate.m
@@ -0,0 +1,28 @@
+function PlotStatusUpdate(app,textUpdate,WhichOne)
+
+%{
+This function adds to the output display
+%}
+textUpdate = string(textUpdate);
+
+Text_size = length(textUpdate);
+if WhichOne == 1
+    OTsize = length(app.LFPOutputText);
+    if Text_size == 1
+        app.LFPOutputText((OTsize(1)+1),1) = textUpdate;
+    else
+        app.LFPOutputText((OTsize(1)+1):(OTsize(1)+Text_size),1) = textUpdate;
+    end
+    app.StatusLFP_TextArea.Value = app.LFPOutputText;
+    scroll(app.StatusLFP_TextArea,'bottom');
+elseif WhichOne == 2
+    OTsize = length(app.DiffOutputText);
+    if Text_size == 1
+        app.DiffOutputText((OTsize(1)+1),1) = textUpdate;
+    else
+        app.DiffOutputText((OTsize(1)+1):(OTsize(1)+Text_size),1) = textUpdate;
+    end
+    app.StatusDiff_TextArea.Value = app.DiffOutputText;
+    scroll(app.StatusDiff_TextArea,'bottom');
+end
+pause(0.1);
